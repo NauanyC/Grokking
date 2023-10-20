02dda9244d77cd8403e3fa659f7e2ae21bf8f7cf
NauanyC <nauanycc@outlook.com.br>
2023-10-19 21:44:01 -0300

    fix: change file to js

--- chapter_07/dijkstra_algorithm.js
+++ chapter_07/dijkstra_algorithm.js
@@ -1,5 +1,5 @@
 // the graph
-const graph = {};
+/*  const graph = {};
 graph["start"] = {};
 graph["start"]["a"] = 6;
 graph["start"]["b"] = 2;
@@ -24,6 +24,52 @@ const parents = {};
 parents["a"] = "start";
 parents["b"] = "start";
 parents["fin"] = null;
+ */
+
+
+//Exercise a
+
+// the graph
+const graph = {};
+graph["start"] = {};
+graph["start"]["a"] = 5;
+graph["start"]["b"] = 2;
+
+graph["a"] = {};
+graph["a"]["c"] = 4;
+graph["a"]["d"] = 2;
+
+graph["b"] = {};
+graph["b"]["a"] = 8;
+graph["b"]["d"] = 7
+
+
+graph["c"] = {};
+graph["c"]["fin"] = 3;
+graph["c"]["d"] = 6
+
+
+graph["d"] = {};
+graph["d"]["fin"] = 1;
+
+graph["fin"] = {};
+
+// The costs table
+const costs = {};
+costs["a"] = 5;
+costs["b"] = 2;
+costs["c"] = Infinity;
+costs["d"] = Infinity;
+costs["fin"] = Infinity;
+
+// the parents table
+const parents = {};
+parents["a"] = "start";
+parents["b"] = "start";
+parents["c"] = null;
+parents["d"] = null;
+parents["fin"] = null;
+
 
 let processed = [];
 
@@ -55,7 +101,7 @@ while (node !== null) {
   const cost = costs[node];
   // Go through all the neighbors of this node
   const neighbors = graph[node];
-  Object.keys(neighbors).forEach(function(n) {
+  Object.keys(neighbors).forEach(function (n) {
     const new_cost = cost + neighbors[n];
     // If it's cheaper to get to this neighbor by going through this node
     if (costs[n] > new_cost) {
@@ -79,7 +125,7 @@ console.log(costs); // { a: 5, b: 2, fin: 6 }
 
 
 console.log("Resulting tables:");
-console.log({parents})
-console.log({costs})
-console.log({graph})
-console.log({processed})
+console.log({ parents })
+console.log({ costs })
+console.log({ graph })
+console.log({ processed })
